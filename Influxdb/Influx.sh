d722a1df03f88e0a3d3ab90620f1ad59c8de9dda
Sarvidal <pawel0526@gmail.com>
2023-01-09 00:53:29 +0100

    Add Influxdb deploy script

---
+++ Influxdb/Influx.sh
@@ -0,0 +1,34 @@
+#!bin/bash
+
+echo ""
+echo "Pull influx v2.2.0 image"
+docker pull influxdb:2.2.0
+echo ""
+sleep 1s
+
+echo ""
+echo "Create catalogs  data, config in /Apps/Influx/"
+mkdir -p /Apps/Influx/data
+mkdir -p /Apps/Influx/config
+chmod 777 /Apps
+echo ""
+sleep 1s
+
+echo ""
+echo "Deploy influx 2.2 container"
+docker run -d -p 8086:8086 \
+      -v /Apps/influx/data:/var/lib/influxdb2 \
+      -v /Apps/influx//config:/etc/influxdb2 \
+      -e DOCKER_INFLUXDB_INIT_MODE=setup \
+      -e DOCKER_INFLUXDB_INIT_USERNAME=admin \
+      -e DOCKER_INFLUXDB_INIT_PASSWORD=admin\
+      -e DOCKER_INFLUXDB_INIT_ORG=3m\
+      -e DOCKER_INFLUXDB_INIT_BUCKET=test \
+      influxdb:2.2.0
+echo ""
+sleep 1s
+
+echo ""
+echo "Influx installed - use http://ip:8086 to get in"
+echo ""
+sleep 1s
